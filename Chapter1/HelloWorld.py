7206a9f2cbe8c33619415243a21b1d5f282c8c93
Alejandro Alexiades <demealexiades@gmail.com>
2021-10-17 17:52:41 +0200

    Create HelloWorld.py

---
+++ Chapter1/HelloWorld.py
@@ -0,0 +1,17 @@
+from typing import Text
+import boto3
+#Explicit Client Configuratio, insert your own Access Keys Credentials
+polly = boto3.client('polly',
+                      region_name='us-west-2',
+                      aws_access_key_id='',
+                      aws_secret_access_key=''
+                      )
+
+result = polly.synthesize_speech(Text='Hello World!',
+OutputFormat='mp3',
+VoiceId='Aditi')
+
+# Save the Audio from the response
+audio = result['AudioStream'].read()
+with open("helloworld.mp3","wb") as file:
+    file.write(audio)
