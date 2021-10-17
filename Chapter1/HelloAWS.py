e70bc0709fba823734ec3bbb6504dd4388523ff6
alexiades <demealexiades@gmail.com>
2021-10-17 23:27:33 +0200

    commit

---
+++ Chapter1/HelloAWS.py
@@ -0,0 +1,23 @@
+from typing import Text
+import boto3
+#Explicit Client Configuratio, insert your own Access Keys Credentials
+polly = boto3.client('polly',
+                      region_name='us-west-2',
+                      aws_access_key_id='',
+                      aws_secret_access_key=''
+                      )
+
+#Implicit Client Configuration
+polly = boto3.client('polly')
+
+result = polly.synthesize_speech(Text='Hello AWS World!',
+OutputFormat='mp3',
+VoiceId='Joanna',
+LexiconNames= [
+            'awsLexicon',
+        ])
+
+# Save the Audio from the response
+audio = result['AudioStream'].read()
+with open("helloworldAws.mp3","wb") as file:
+    file.write(audio)
